f6d18920f0febbf3aaff2146005e20a5efbdd4cf
jrabbit <jackjrabbit+github@gmail.com>
2014-07-25 23:38:48 -0400

    clean up and delete empathy logs

--- kastoro.py
+++ kastoro.py
@@ -1,32 +1,49 @@
 #!/usr/bin/env python
 # kastoro (c) Jrabbit (Jack Laxson) released under gpl v3
-
-
-
+import shutil
+import os.path
 #temporary settings until commandline option lib is chosen.
 turn_off_logging = True
 delete_existing_logs = False
 
-def disable_empathy():
-    try:
-        from gi.repository import Gio
-    except ImportError:
-        print "Currently targeting GTK."
-    log_setting = Gio.Settings.new("org.freedesktop.Telepathy.Logger")
-    if not log_setting.get_boolean("enabled"):
-        #Then we're done already
-        return True
-    
 
-def disable_logs(all, *args, **kawargs):
-    pass
+#utilities
+def delete_directory(nice_path):
+    """Take expandable path. Deletes folder and paths below"""
+    #translate ~ and any weird vars
+    path = os.path.expandvars(os.path.expanduser(nice_path))
+    shutil.rmtree(path)
 
-def delete_logs(all, *args, **kwargs):
-    pass
+class Empathy(object):
+    needs_GIO = True
+    needs_augeas = False
+    def disable():
+        try:
+            from gi.repository import Gio
+        except ImportError:
+            print "Currently targeting GTK."
+
+        log_setting = Gio.Settings.new("org.freedesktop.Telepathy.Logger")
+        if not log_setting.get_boolean("enabled"):
+            #Then we're done already
+            return True
+        else:
+            log_setting.set_boolean("enabled", False)
+            return True
+    def delete():
+        delete_directory("~/.local/share/TpLogger/logs")
+
+def disable_logs(every, *args, **kawargs):
+    if every:
+        Empathy.disable()
+
+def delete_logs(every, *args, **kwargs):
+    if every:
+        Empathy.delete()
 
 def main():
     if turn_off_logging:
-        disable_logs()
+        disable_logs(every=True)
     if delete_existing_logs:    
         delete_logs()
 
